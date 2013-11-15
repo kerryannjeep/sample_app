77906cc0ee7fc8f1755e80a73311879ec894d3f7
kerryannjeep <kschaffer@marketingassociates.com>
2013-11-14 20:35:29 -0500

    effed up static pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -3,3 +3,16 @@ require 'spec_helper'
 describe StaticPagesController do
 
 end
+
+
+class StaticPagesController < ApplicationController
+
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+end
